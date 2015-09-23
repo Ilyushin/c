a28fdd3739b61e875642ec4e7d8872189c853d12
Evgeniy Ilyushin <john.ilyushin@gmail.com>
2015-09-23 10:05:39 +0300

    Add an example of allocation memory

--- mem_alloc.cpp
+++ mem_alloc.cpp
@@ -1,7 +1,30 @@
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 
 int main(){
+
+    char *name;
+
+    /* allocate memory dynamically */
+    name = (char *)malloc( 30 * sizeof(char) );
+
+    if( name == NULL )
+    {
+        fprintf(stderr, "Error - unable to allocate required memory\n");
+    }
+    else
+    {
+        strcpy( name, "Test allocation memory.");
+    }
+
+    printf("Name = %s\n", name );
+
+    /* release memory using free() function */
+    free(name);
+
+    return 0;
 
 }
 
